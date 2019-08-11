9023a64b680fcdea1fde1f56e24d33b61ca67121
João Gabriel Machado <joaogabriel013@gmail.com>
2019-08-11 13:13:38 -0300

    Adding the button to clear the table

--- src/Components/InputStocks.js
+++ src/Components/InputStocks.js
@@ -3,11 +3,15 @@ import { Card, CardBody, CardHeader, CardTitle, Row, Col, FormGroup, Input } fro
 import { stockListService } from '../Services';
 
 export default class InputStocks extends Component {
-    submitForm(event){
+    submitForm(event) {
         event.preventDefault();
         const stock = { 'code': this.code.value, 'quantity': this.quantity.value, 'price': this.price.value };
         stockListService.sendStocks(stock);
         this.code.value = ""; this.quantity.value = ""; this.price.value = "";
+    }
+
+    clearTable() {
+        stockListService.clearStocks();
     }
     
     render() {
@@ -23,23 +27,24 @@ export default class InputStocks extends Component {
                                 <Col lg="4" md="4" sm="4">
                                     <FormGroup>
                                         <label>Codigo</label>
-                                        <Input type="text" placeholder="Codigo" className="form-control" innerRef={(input) => this.code = input}/>
+                                        <Input type="text" placeholder="Codigo" className="form-control" innerRef={(input) => this.code = input} required={ true }/>
                                     </FormGroup>
                                 </Col>
                                 <Col lg="4" md="4" sm="4">
                                     <FormGroup>
                                         <label>Quantidade</label>
-                                        <Input type="number" min="1" placeholder="Quantidade" className="form-control" innerRef={(input) => this.quantity = input}/>
+                                        <Input type="number" min="1" placeholder="Quantidade" className="form-control" innerRef={(input) => this.quantity = input} required={ true }/>
                                     </FormGroup>
                                 </Col>
                                 <Col lg="4" md="4" sm="4">
                                     <FormGroup>
                                         <label>Price</label>
-                                        <Input type="number" step="0.01" min="1" placeholder="Preco" className="form-control" innerRef={(input) => this.price = input}/>
+                                        <Input type="number" step="0.01" min="1" placeholder="Preco" className="form-control" innerRef={(input) => this.price = input} required={ true }/>
                                     </FormGroup>
                                 </Col>
                             </Row>
                             <Input type="submit" value="Enviar"/>
+                            <button className="form-control" type="submit" onClick={this.clearTable.bind(this)}>Limpar tabela</button>
                         </form>
                     </CardBody>
                 </Card>
